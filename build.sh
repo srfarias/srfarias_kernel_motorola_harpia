005b61bdb7df8849493e47f3ced407af4ed8762d
srfarias <ericfari15@gmail.com>
2018-10-03 03:09:41 -0300

    build script updates

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 KDIR=$PWD
 SRKDIR=~/srfariaskmau
 TCDIR=~/arm-eabi-5.3/bin/arm-eabi-
-DATE=$(date +"%m%d%y")
+DATE=$(date +"%d%m%y")
 KNAME="SrfariasKernel"
 
 export ARCH=arm
@@ -11,41 +11,33 @@ export SUBARCH=arm
 export CROSS_COMPILE=$TCDIR
 export LD_LIBRARY_PATH="/usr/local/lib"
 sudo ldconfig
-export USE_CCACHE=1
+export CONFIG_CCACHE=y
 export DEVICE="harpia"
 export KBUILD_BUILD_USER="srfarias"
 export KBUILD_BUILD_HOST="mau"
 export FINAL_ZIP="$KNAME"-"$DEVICE"_"$DATE".zip
 
 # Sanity check to avoid using erroneous binaries
+rm -f $SRKDIR/system/lib/modules/pronto/pronto_wlan.ko
+rm -f $SRKDIR/tools/zImage
+rm -f $SRKDIR/tools/dt.img
 rm -f arch/arm/boot/dts/*.dtb
 rm -f arch/arm/boot/dt.img
 make clean && make mrproper
 
 echo "==> Making kernel binary..."
 make harpia_defconfig
-make -j3 zImage |& tee -a fail.log
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then
-	echo "!!! Kernel compilation failed, can't continue !!!"
-	exit 2
-fi
+make -j12 zImage |& tee -a fail.log
 
 echo "=> Making DTBs..."
-make -j3 dtimage|& tee -a fail.log
+make -j12 dtimage
 
 echo "=> Making modules..."
-make -j3 modules |& tee -a fail.log
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then
-	echo "Module compilation failed, can't continue."
-	exit 1
-fi
+make -j12 modules
+
 rm -rf srfarias_install
 mkdir -p srfarias_install
-make -j3 modules_install INSTALL_MOD_PATH=srfarias_install INSTALL_MOD_STRIP=1 |& tee -a fail.log
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then
-	echo "Module installation failed, can't continue."
-	exit 1
-fi
+make -j12 modules_install INSTALL_MOD_PATH=srfarias_install INSTALL_MOD_STRIP=1
 
 echo "==> Kernel compilation completed"
 
@@ -53,7 +45,8 @@ echo "==> Making Flashable zip"
 
 echo "=> Finding modules"
 
-find srfarias_install/ -name '*.ko' -type f -exec cp '{}' "$SRKDIR/system/lib/modules/" \;
+find srfarias_install/ -name '*.ko' -type f -exec cp '{}' "$SRKDIR/system/lib/modules/pronto/" \;
+mv $SRKDIR/system/lib/modules/pronto/wlan.ko $SRKDIR/system/lib/modules/pronto/pronto_wlan.ko
 
 cp  $KDIR/arch/$ARCH/boot/zImage $SRKDIR/tools/
 
